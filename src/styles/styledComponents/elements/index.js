4e30578c2ef4c9855906d8fc0a200fee66695e53
fujisawakyle <fujisawakyle@gmail.com>
2018-08-19 11:05:03 -0700

    add color assignments, font-size to base.scss, add corresponding rules to elements index.js

--- src/styles/styledComponents/elements/index.js
+++ src/styles/styledComponents/elements/index.js
@@ -1,41 +1,93 @@
 import styled from "styled-components";
 
 import { fromThemeProps } from "../../../helpers/utilities";
+import media from "../../../helpers/media";
 
 export const H1 = styled.h1`
-    font-family: ${fromThemeProps("h1Font")};
-    font-weight: ${fromThemeProps("h1Weight")};
+  color: ${fromThemeProps("h1Color")};
+  font-family: ${fromThemeProps("h1Font")};
+  font-size: ${fromThemeProps("h1FontSizeMobile")};
+  font-weight: ${fromThemeProps("h1Weight")};
+
+  ${media.xs`
+    font-size: ${fromThemeProps("h1FontSizeDesktop")};
+  `}
 `;
 
 export const H2 = styled.h2`
-    font-family: ${fromThemeProps("h2Font")};
-    font-weight: ${fromThemeProps("h2Weight")};
+  color: ${fromThemeProps("h2Color")};
+  font-family: ${fromThemeProps("h2Font")};
+  font-size: ${fromThemeProps("h2FontSizeMobile")};
+  font-weight: ${fromThemeProps("h2Weight")};
+
+  ${media.xs`
+    font-size: ${fromThemeProps("h2FontSizeDesktop")};
+  `}
 `;
 
 export const H3 = styled.h3`
-    font-family: ${fromThemeProps("h3Font")};
-    font-weight: ${fromThemeProps("h3Weight")};
+  color: ${fromThemeProps("h3Color")};
+  font-family: ${fromThemeProps("h3Font")};
+  font-size: ${fromThemeProps("h3FontSizeMobile")};
+  font-weight: ${fromThemeProps("h3Weight")};
+
+  ${media.xs`
+    font-size: ${fromThemeProps("h3FontSizeDesktop")};
+  `}
 `;
 
 export const H4 = styled.h4`
-    font-family: ${fromThemeProps("h4Font")};
-    font-weight: ${fromThemeProps("h4Weight")};
+  color: ${fromThemeProps("h4Color")};
+  font-family: ${fromThemeProps("h4Font")};
+  font-size: ${fromThemeProps("h4FontSizeMobile")};
+  font-weight: ${fromThemeProps("h4Weight")};
+
+  ${media.xs`
+    font-size: ${fromThemeProps("h4FontSizeDesktop")};
+  `}
 `;
 export const H5 = styled.h5`
-    font-family: ${fromThemeProps("h5Font")};
-    font-weight: ${fromThemeProps("h5Weight")};
+  color: ${fromThemeProps("h5Color")};
+  font-family: ${fromThemeProps("h5Font")};
+  font-size: ${fromThemeProps("h5FontSizeMobile")};
+  font-weight: ${fromThemeProps("h5Weight")};
+
+  ${media.xs`
+    font-size: ${fromThemeProps("h5FontSizeDesktop")};
+  `}
 `;
 
 export const P = styled.p`
-    font-family: ${fromThemeProps("pFont")};
-    font-weight: ${fromThemeProps("pWeight")};
+  color: ${fromThemeProps("h5Color")};
+  font-family: ${fromThemeProps("pFont")};
+  font-size: ${fromThemeProps("pFontSizeMobile")};
+  font-weight: ${fromThemeProps("pWeight")};
+
+  ${media.xs`
+    font-size: ${fromThemeProps("pFontSizeDesktop")};
+  `}
 `;
 
-export const A = styled.a``;
+export const A = styled.a`
+  color: ${fromThemeProps("aColor")};
+  font-family: ${fromThemeProps("aFont")};
+  font-size: ${fromThemeProps("aFontSizeMobile")};
+  font-weight: ${fromThemeProps("aWeight")};
+
+  ${media.xs`
+    font-size: ${fromThemeProps("aFontSizeDesktop")};
+  `}
+`;
 
 export const Button = styled(A)`
-    font-family: ${fromThemeProps("buttonFont")};
-    font-weight: ${fromThemeProps("buttonWeight")};
+  color: ${fromThemeProps("buttonColor")};
+  font-family: ${fromThemeProps("buttonFont")};
+  font-size: ${fromThemeProps("buttonFontSizeMobile")};
+  font-weight: ${fromThemeProps("buttonWeight")};
+
+  ${media.xs`
+    font-size: ${fromThemeProps("buttonFontSizeDesktop")};
+  `}
 `
 
 export const Ol = styled.ol``;
